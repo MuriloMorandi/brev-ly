fe8320f28aebbb2aabac9158c1d78248db37b759
Murilo Morandi Alexandre <murilomorandialexandre2011@gmail.com>
2025-10-01 17:04:53 -0300

    fix(web): Centralizado conteudo da página de link não encontrado e de redirecionamento

--- web/src/pages/redirect-page.tsx
+++ web/src/pages/redirect-page.tsx
@@ -6,7 +6,7 @@ import { api } from "../libs/api";
 export function RedirectPage() {
     const { shortUrl } = useParams();
     const [url, setUrl] = useState('')
-  let navigate = useNavigate();
+    let navigate = useNavigate();
 
     useEffect(()=>{
         api.get(`link/${shortUrl}`)
@@ -21,28 +21,30 @@ export function RedirectPage() {
     }, [])
     
 
-    return(
-        <div className="flex flex-col gap-4 bg-gray-100 overflow-hidden w-full md:w-[580px] rounded-lg justify-center items-center text-center py-16 px-8">
-            <span className="text-xl text-gray-600">Redirecionando...</span>
-            <span>
-                <p className="text-gray-500">
-                    O link será aberto automaticamente em alguns instantes. 
-                </p>
-                <p className="text-gray-500">
-                    Não foi redirecionado? 
-                    {' '}
-                    <a href={url} target='_blank' className="text-blue-base">
-                        Acesse aqui
-                    </a>
-                </p>
-            </span>
-            
-            <img
-                src={LogoIcon}
-                alt="Logo"
-                className="w-12 h-12 object-contain"
-                loading="lazy"
-            />
+    return (
+        <div className="flex items-center justify-center min-h-screen">
+            <div className="flex flex-col gap-4 bg-gray-100 overflow-hidden w-full md:w-[580px] rounded-lg justify-center items-center text-center py-16 px-8">
+                <span className="text-xl text-gray-600">Redirecionando...</span>
+                <span>
+                    <p className="text-gray-500">
+                        O link será aberto automaticamente em alguns instantes. 
+                    </p>
+                    <p className="text-gray-500">
+                        Não foi redirecionado? 
+                        {' '}
+                        <a href={url} target='_blank' className="text-blue-base">
+                            Acesse aqui
+                        </a>
+                    </p>
+                </span>
+                
+                <img
+                    src={LogoIcon}
+                    alt="Logo"
+                    className="w-12 h-12 object-contain"
+                    loading="lazy"
+                />
+            </div>
         </div>
     )
 }
